f02dfea7911630e3be4b85492ba1a8a294a8b12e
aselka27 <asjumalieva@gmail.com>
2023-03-14 16:20:00 +0600

    Initial Commit

---
+++ MVVMDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVMDemo
+//
+//  Created by саргашкаева on 14.03.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
